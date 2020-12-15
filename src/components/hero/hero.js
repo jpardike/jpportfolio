3f4be9f5921a07b181125c2ad2fdcd0e1c7e0980
Josh Pardike <44354315+jpardike@users.noreply.github.com>
2020-12-14 23:57:27 -0800

    Changed hero title

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -23,7 +23,7 @@ class Hero extends React.Component {
                             opacity: this.state.divViz ? 1 : 0,
                             transition: 'opacity .6s .5s ease-out, transform .7s .5s ease-out'
                         }}>
-                            <h1>Front-end Developer</h1>
+                            <h1>Software Engineer</h1>
                             <h4><i className="fab fa-react"  id="hero-icon"></i><i className="fab fa-bootstrap" id="hero-icon"></i><i className="fab fa-html5" id="hero-icon"></i><i className="fab fa-css3-alt" id="hero-icon"></i></h4>
                         </div>
                     </VizSensor>
