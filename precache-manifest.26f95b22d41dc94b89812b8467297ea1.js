ad03a0c1396744e939d9de68e8c35a16ab00b01d
jpardike <jpardike@gmail.com>
2020-12-16 23:58:54 -0800

    Updates

---
+++ precache-manifest.26f95b22d41dc94b89812b8467297ea1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c085f35931fcf21cf12b8254f392bc9",
+    "url": "/jpportfolio/index.html"
+  },
+  {
+    "revision": "fe17539fda3d394507f0",
+    "url": "/jpportfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1d17803b4c180787129e",
+    "url": "/jpportfolio/static/css/main.a15ba3cb.chunk.css"
+  },
+  {
+    "revision": "fe17539fda3d394507f0",
+    "url": "/jpportfolio/static/js/2.11cf166c.chunk.js"
+  },
+  {
+    "revision": "da27bfe0690a83ec4d49b78fc2f5a1c1",
+    "url": "/jpportfolio/static/js/2.11cf166c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d17803b4c180787129e",
+    "url": "/jpportfolio/static/js/main.58cc6f31.chunk.js"
+  },
+  {
+    "revision": "62d38c54612d1a64e173",
+    "url": "/jpportfolio/static/js/runtime-main.df8e3149.js"
+  },
+  {
+    "revision": "8a45497389af0bffe5cd3f3d30f7c38f",
+    "url": "/jpportfolio/static/media/20171209_172208_85.8a454973.jpg"
+  },
+  {
+    "revision": "87588de92c2740b5a8bf48993491b399",
+    "url": "/jpportfolio/static/media/Screenshot_2020-01-27 Josh's Guitars.87588de9.png"
+  },
+  {
+    "revision": "0ab022f7cbd6da8f1fb8fdcd10bcd106",
+    "url": "/jpportfolio/static/media/Screenshot_2020-01-27 https jpardike github io.0ab022f7.png"
+  },
+  {
+    "revision": "6e30c3288eead8da61a5597df15b87bc",
+    "url": "/jpportfolio/static/media/turned-on-black-laptop-computer-on-table-2148217.6e30c328.jpg"
+  }
+]);
