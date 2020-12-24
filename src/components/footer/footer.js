06aa9de611987172ad07cc95eec17d18576d20ed
jpardike <jpardike@gmail.com>
2020-12-23 23:58:31 -0800

    Updates Footer text and color

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component{
         return (
             <div className="container-fluid" id="footer">
                 <footer className="justify-content-center text-center">
-                    <h5 className="p-3 footer-text">This site was made using ReactJS and Bootstrap</h5>
+                    <h5 className="p-3 footer-text">This site was made using React</h5>
                     <a className="p-3 footer-text" href="https://github.com/jpardike/jpportfolio/tree/gh-pages">Check out the code for this site!</a>
                 </footer>
             </div>
