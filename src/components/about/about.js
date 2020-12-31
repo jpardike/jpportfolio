a0c0a1ac24a01af0513f32d959075b861bd0c966
jpardike <jpardike@gmail.com>
2020-12-30 21:59:42 -0800

    Updates to about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -36,7 +36,7 @@ class About extends React.Component{
                         transform: this.state.divViz ? 'translate(0%,0%)' : 'translate(0%,10%)',
                         opacity: this.state.divViz ? 1 : 0,
                         transition: 'opacity .6s 1s ease-out, transform .7s 1s ease-out'
-                    }}>I am a Full-Stack Software Engineer with a background in Team Management. I use my combined experiences to build user friendly web applications while maintaining an organized work flow and time management for myself and my team. I aspire to web apps that solve everyday problems, from software that can strengthen a community of people, to a server that distributes data to those in need, while continuing my path of learning new technologies.</p>
+                    }}>I am a Full-Stack Software Engineer with a background in Team Management. I use my combined experiences to build user friendly web applications while maintaining an organized work flow and time management for myself and my team. My goal is to use my skills to create meaningful applications that are easy to use, and to continue learning.</p>
                     </div>
                     </VizSensor>
                 </div>
