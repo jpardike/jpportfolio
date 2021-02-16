cd96dcb9aadef4443a2f05095718e1bbe5c11a0b
jpardike <jpardike@gmail.com>
2021-02-15 16:20:09 -0800

    Adds links to github and linkedin on about component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
-class Footer extends React.Component{
-    render() {
-        return (
-            <div className="container-fluid" id="footer">
-                <footer className="justify-content-center text-center">
-                    <h5 className="p-3 footer-text">This site was made using React</h5>
-                    <a className="p-3 footer-text" href="https://github.com/jpardike/jpportfolio/tree/josh">Check out the code for this site!</a>
-                </footer>
-            </div>
-        )
-    }
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid" id="footer">
+        <footer className="justify-content-center text-center">
+          <h5 className="p-3 footer-text">Check out the code for this site!</h5>
+          <a
+            className="p-3 footer-text footer-link"
+            href="https://github.com/jpardike/jpportfolio/tree/josh"
+          >
+            <i class="fab fa-github"></i>
+          </a>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default Footer;
